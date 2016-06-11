9ff97f628299952fbe5146f08fa3f192942b8923
STEPHANYA CASANOVA MARROQUIN <savacano_28@live.com>
2016-06-11 16:33:51 -0500

    Update P1_suma_scatter.c

--- P1_suma_scatter.c
+++ P1_suma_scatter.c
@@ -1,6 +1,12 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <sys/time.h>
+
 #define MAX 100
 
 int main(int argc, char** argv) {
+        double totalT; 
         int rank, size, i,stride, source,tag=1,total=0;
         float a[MAX];
         float acum = 0;
@@ -8,6 +14,7 @@ int main(int argc, char** argv) {
 
         for (i = 1; i <= 100; i++)
         a[ i - 1 ] = i;
+
         MPI_Init(&argc,&argv);
         MPI_Comm_size(MPI_COMM_WORLD, &size);
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -20,14 +27,19 @@ int main(int argc, char** argv) {
                 scounts[i] = stride;
         }
 
+        double tstart = MPI_Wtime();
         MPI_Scatterv(a,scounts,disp,MPI_FLOAT,b,stride,MPI_FLOAT,0,MPI_COMM_WORLD);
+        double tfinish = MPI_Wtime();
+        double TotalTime = tfinish - tstart;
+
         acum = 0.0;
         for (i = 0; i < stride; i++)
                 acum += b[i];
-        printf("Subtotal %lf en nodo %d\n", acum, rank);
+        printf("Subtotal %lf en nodo %d tiempo %g\n", acum, rank,TotalTime);
 
-        MPI_Reduce(&acum,b,1,MPI_FLOAT,MPI_SUM,0,MPI_COMM_WORLD);
-        if (rank == 0) printf("TOTAL: %lf\n",b[0]);
+        MPI_Reduce(&TotalTime,&totalT,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+        MPI_Reduce(&acum,&total,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+        if (rank == 0)
+                printf("Total: %d Tiempo: %g \n", total,totalT);
         MPI_Finalize();
 }
-
